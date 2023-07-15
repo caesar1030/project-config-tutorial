117be805ab8cf1fedefc2b5ebf377893354e6114
zlx454545 <zlx454545@gmail.com>
2023-07-15 22:56:05 +0900

    Feat: 사칙연산 함수 구현

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import add from './add.js';
+import divide from './divide.js';
+import multiply from './multiply.js';
+import subtract from './subtract.js';
+
+console.log(add(3, 5));
+console.log(subtract(5, 3));
+
+console.log(multiply(8, 9));
+
+console.log(divide(6, 3));
