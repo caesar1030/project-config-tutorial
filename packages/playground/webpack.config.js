d6ee7e68acb9c21a860e2d09dcf27c45aa2eec90
zlx454545 <zlx454545@gmail.com>
2023-07-26 18:12:31 +0900

    Chore: handle bar loader를 이용해 동적으로 생성되는 html에 정보 기입

--- packages/playground/webpack.config.js
+++ packages/playground/webpack.config.js
@@ -6,7 +6,22 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, './dist'),
+    clean: true,
   },
   mode: 'none',
-  plugins: [new HtmlWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.hbs$/,
+        use: ['handlebars-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '플레이그라운드',
+      template: 'src/index.hbs',
+      description: '웹팩과 모노레포를 학습하기 위한 프로젝트',
+    }),
+  ],
 };
