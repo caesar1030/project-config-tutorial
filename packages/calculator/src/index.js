7945b540417a49c52ac922e8c06ade224f01551f
zlx454545 <zlx454545@gmail.com>
2023-08-01 00:48:27 +0900

    Edit: 사칙연산 함수 수정

--- packages/calculator/src/index.js
+++ packages/calculator/src/index.js
@@ -1,33 +1,44 @@
+const pipe =
+  (...funcs) =>
+  (arg) =>
+    funcs.reduce((result, func) => func(result), arg);
+
+const validateAndConvert = (operand) => {
+  const number = Number(operand);
+  if (isNaN(number)) throw new Error('입력값이 숫자가 아닙니다.');
+
+  return number;
+};
+
+const validateNotZero = (number) => {
+  if (number === 0) throw new Error('0으로 나눌 수 없습니다.');
+  return number;
+};
+
 export const add = (initialValue = 1, ...restNumbers) => {
-  return restNumbers.reduce((acc, cur) => {
-    const operand = Number(cur);
-    if (isNaN(operand)) throw new Error('숫자로 전환 불가능한 타입');
-
-    return (acc += operand);
-  }, initialValue);
+  return restNumbers.reduce(
+    (acc, cur) => acc + validateAndConvert(cur),
+    initialValue
+  );
 };
+
 export const subtract = (initialValue = 1, ...restNumbers) => {
-  return restNumbers.reduce((acc, cur) => {
-    const operand = Number(cur);
-    if (isNaN(operand)) throw new Error('숫자로 전환 불가능한 타입');
-
-    return (acc -= operand);
-  }, initialValue);
+  return restNumbers.reduce(
+    (acc, cur) => acc - validateAndConvert(cur),
+    initialValue
+  );
 };
+
 export const multiply = (initialValue = 1, ...restNumbers) => {
-  return restNumbers.reduce((acc, cur) => {
-    const operand = Number(cur);
-    if (isNaN(operand)) throw new Error('숫자로 전환 불가능한 타입');
-
-    return (acc *= operand);
-  }, initialValue);
+  return restNumbers.reduce(
+    (acc, cur) => acc * validateAndConvert(cur),
+    initialValue
+  );
 };
-export const divide = (initialValue = 1, ...restNumbers) => {
-  return restNumbers.reduce((acc, cur) => {
-    const operand = Number(cur);
-    if (isNaN(operand)) throw new Error('숫자로 전환 불가능한 타입');
-    if (operand === 0) throw new Error('0으로 나눌 수 없습니다.');
 
-    return (acc *= operand);
-  }, initialValue);
+export const divide = (initialValue = 1, ...restNumbers) => {
+  return restNumbers.reduce(
+    (acc, cur) => acc / pipe(validateAndConvert, validateNotZero)(cur),
+    initialValue
+  );
 };
