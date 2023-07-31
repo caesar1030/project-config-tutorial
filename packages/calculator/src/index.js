be83ec422c96cdfb3db720cd8cbe348d50fa3abd
zlx454545 <zlx454545@gmail.com>
2023-07-31 23:55:23 +0900

    Edit: 함수 수정

--- packages/calculator/src/index.js
+++ packages/calculator/src/index.js
@@ -1,4 +1,33 @@
-export const add = (a, b) => a + b;
-export const subtract = (a, b) => a - b;
-export const multiply = (a, b) => a * b;
-export const divide = (a, b) => a / b;
+export const add = (initialValue = 1, ...restNumbers) => {
+  return restNumbers.reduce((acc, cur) => {
+    const operand = Number(cur);
+    if (isNaN(operand)) throw new Error('숫자로 전환 불가능한 타입');
+
+    return (acc += operand);
+  }, initialValue);
+};
+export const subtract = (initialValue = 1, ...restNumbers) => {
+  return restNumbers.reduce((acc, cur) => {
+    const operand = Number(cur);
+    if (isNaN(operand)) throw new Error('숫자로 전환 불가능한 타입');
+
+    return (acc -= operand);
+  }, initialValue);
+};
+export const multiply = (initialValue = 1, ...restNumbers) => {
+  return restNumbers.reduce((acc, cur) => {
+    const operand = Number(cur);
+    if (isNaN(operand)) throw new Error('숫자로 전환 불가능한 타입');
+
+    return (acc *= operand);
+  }, initialValue);
+};
+export const divide = (initialValue = 1, ...restNumbers) => {
+  return restNumbers.reduce((acc, cur) => {
+    const operand = Number(cur);
+    if (isNaN(operand)) throw new Error('숫자로 전환 불가능한 타입');
+    if (operand === 0) throw new Error('0으로 나눌 수 없습니다.');
+
+    return (acc *= operand);
+  }, initialValue);
+};
